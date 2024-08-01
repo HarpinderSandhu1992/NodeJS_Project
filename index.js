4956b3ab51afc9a49714cdd065e4261db6913f59
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-07-31 20:03:26 -0600

    Employers using GET/POST methods

--- index.js
+++ index.js
@@ -13,11 +13,35 @@ sequelize
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/employers", (req, res) => {
+  Employer.findAll()
+    .then((employers) => {
+      res.send(employers);
+    })
+    .catch((error) => res.send(error, 500));
+});
+
+app.post("/employers", (req, res) => {
+  // Get submitted data
+  const new_employer = {
+    Name: req.body.name,
+    Salary: req.body.salary,
+    Department: req.body.department,
+  };
+  // Insert new employer data into database
+  Employer.create(new_employer)
+    .then((employer) => {
+      res.send(employer, 201);
+    })
+    .catch((error) => res.send(error, 500));
+});
 app.listen(3000, function () {
   console.log("Server connected");
 });
